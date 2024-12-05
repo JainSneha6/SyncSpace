4becbfd72c2f9ed35772ffcf8e4aa6fca8da1197
Siddhartha Chakrabarty <csiddhartha2004@gmail.com>
2024-12-05 21:21:47 +0530

    Scene ho gaye

--- backend/server.js
+++ backend/server.js
@@ -33,25 +33,25 @@ let pptData = {};
 app.post('/uploadPpt', upload.single('file'), async (req, res) => {
   const { roomId } = req.body; // Extract roomId from the request body
   const pptFile = req.file; // Access the uploaded file
-
+  console.log('1')
   if (!pptFile) {
     return res.status(400).json({ error: 'No file uploaded' });
   }
-
+  console.log('2')
   if (!roomId) {
     return res.status(400).json({ error: 'Room ID is required' });
   }
-
+  console.log('3')
   try {
     // Prepare FormData to send to Flask backend
     const formData = new FormData();
     formData.append('file', pptFile.buffer, pptFile.originalname); // Attach file buffer with original name
-
+    console.log('4')
     // Send the file to Flask backend for processing
     const response = await axios.post('https://syncspace-pkal.onrender.com/upload', formData, {
       headers: formData.getHeaders(),
     });
-
+    console.log('5')
     const { slides, folder, pdf } = response.data;
 
     // Store the slides data for the room
