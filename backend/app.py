1e9abe186cc77c89b4f4a3d15033c7cf93488098
Sneha Jain <jainsnehasj6@gmail.com>
2024-12-11 19:13:27 +0530

    I am so done

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,6 @@ import requests  # pip install requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import io
-import base64
-from io import BytesIO
-from PIL import Image
-import google.generativeai as genai
 
 app = Flask(__name__)
 CORS(app)
@@ -17,9 +13,6 @@ BASE_URL = "https://api.pdf.co/v1"
 Password = ""
 Pages = ""
 
-API_KEY = "AIzaSyC6iqFmmBrHeAzOu4VSgO7SYCkNtmwCZM8"
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
 
 def upload_file_to_pdf_co(file_stream, file_name):
     """Uploads file to PDF.co directly from memory and returns the uploaded file URL."""
@@ -91,47 +84,6 @@ def upload_ppt():
         }), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
-@app.route('/upload_img', methods=['POST'])
-def upload_image():
-    if request.is_json:
-        try:
-            data = request.get_json()
-            image_data = data.get('image')
-
-            if not image_data:
-                return jsonify({"error": "No image data"}), 400
-
-            # Debugging: Print the first 100 characters of the base64 string to ensure it's correct
-            print(f"Base64 Image Data (First 100 characters): {image_data[:100]}")
-
-            # Decode the image from base64
-            image_data = image_data.split(",")[1]  # Remove the base64 prefix
-            image = Image.open(BytesIO(base64.b64decode(image_data)))
-
-            # Debugging: Verify if the image was opened successfully
-            print(f"Image successfully opened: {image}")
-
-            # Save the image to the server
-            image.save("screenshot.png")
-            return jsonify({"message": "Screenshot received and saved successfully"})
-        except Exception as e:
-            return jsonify({"error": f"Failed to process the image: {str(e)}"}), 500
-    else:
-        return jsonify({"error": "Invalid request format"}), 400
-
-def simplify_text(text):
-    prompt = (
-        "Anal:\n"
-        f"'{text}'"
-    )
-    try:
-        response = model.generate_content([prompt])
-        simplified_text = response.text.replace('**','').replace('*','')
-        return simplified_text
-    except Exception as e:
-        print(f"Error simplifying text: {e}")
-        return "Error simplifying text."
 
 
 if __name__ == '__main__':
